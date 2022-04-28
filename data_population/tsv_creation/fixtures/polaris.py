5366ef513a8dbc0bfad25d67b1ac0d69afb8a037
simontunmore <stunmore@bink.com>
2022-04-28 17:44:54 +0100

    style

--- data_population/tsv_creation/fixtures/polaris.py
+++ data_population/tsv_creation/fixtures/polaris.py
@@ -87,7 +87,10 @@ def generate_polaris_type_key_values(data_config: DataConfig) -> dict[int, dict]
             15: f"retailer_{randint(1, total_retailers)}",  # retailer_slug
             16: f"reward_{randint(1, total_campaigns)}",  # reward_slug
             17: str(uuid4()),  # account_holder_uuid,
-            18: randint(1, (data_config.preloaded_pending_rewards + data_config.jigsaw_pending_rewards),)  # pending_reward_id,
+            18: randint(
+                1,
+                (data_config.preloaded_pending_rewards + data_config.jigsaw_pending_rewards),
+            ),  # pending_reward_id,
         },
     }
     return polaris_task_type_keys
