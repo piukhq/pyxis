550591768fb4923871ebfe71b49bdc9f59b55e78
simontunmore <stunmore@bink.com>
2022-04-29 12:44:20 +0100

    docs

--- data_population/data_config.py
+++ data_population/data_config.py
@@ -3,14 +3,14 @@ from dataclasses import dataclass
 
 @dataclass
 class DataConfig:
-    preloaded_retailers: int
-    jigsaw_retailers: int
+    preloaded_retailers: int  # aka 'Fatface'/FT1 retailers
+    jigsaw_retailers: int  # aka 'ASOS'/FT2 retailers
     account_holders: int
     campaigns_per_retailer: int
     earn_rule_per_campaign: int
     allocated_rewards: int  # number of rewards already allocated to account_holders
-    preloaded_pending_rewards: int  # number of rewards already promised to account_holders (pending)
-    jigsaw_pending_rewards: int  # number of rewards already promised to account_holders (pending)
+    preloaded_pending_rewards: int  # number of rewards already promised to account_holders (pending) (FT1 retailers)
+    jigsaw_pending_rewards: int  # number of rewards already promised to account_holders (pending) (FT2 retailers)
     spare_rewards: int  # number of spare rewards not yet allocated or promised to account_holders
     transactions: int
     reward_updates: int
@@ -18,40 +18,40 @@ class DataConfig:
 
 data_configs = {
     "benchmark": DataConfig(
-        preloaded_retailers=2,
-        jigsaw_retailers=3,
+        preloaded_retailers=2,  # aka 'Fatface'/FT1 retailers
+        jigsaw_retailers=3,  # aka 'ASOS'/FT2 retailers
         campaigns_per_retailer=1,
         earn_rule_per_campaign=1,
         account_holders=200000,
         allocated_rewards=1000,
-        preloaded_pending_rewards=729,
-        jigsaw_pending_rewards=321,
+        preloaded_pending_rewards=729,  # pending rewards for Fatface/FT1 retailers
+        jigsaw_pending_rewards=321,  # pending rewards for ASOS/FT2 retailers
         spare_rewards=0,
         transactions=2000,
         reward_updates=2000,
     ),
     "peak": DataConfig(
-        preloaded_retailers=3,
-        jigsaw_retailers=2,
+        preloaded_retailers=3,  # aka 'Fatface'/FT1 retailers
+        jigsaw_retailers=2,  # aka 'ASOS'/FT2 retailers
         campaigns_per_retailer=1,
         earn_rule_per_campaign=1,
         account_holders=390000,
         allocated_rewards=210000,
-        preloaded_pending_rewards=42000,
-        jigsaw_pending_rewards=42000,
+        preloaded_pending_rewards=105000,  # pending rewards for Fatface/FT1 retailers
+        jigsaw_pending_rewards=105000,  # pending rewards for ASOS/FT2 retailers
         spare_rewards=100000,
         transactions=561600,
         reward_updates=0,
     ),
     "test": DataConfig(
-        preloaded_retailers=5,
-        jigsaw_retailers=5,
+        preloaded_retailers=5,  # aka 'Fatface'/ FT1 retailers
+        jigsaw_retailers=5,  # aka 'ASOS'/FT2 retailers
         campaigns_per_retailer=1,
         earn_rule_per_campaign=1,
         account_holders=1000,
         allocated_rewards=300,
-        preloaded_pending_rewards=200,
-        jigsaw_pending_rewards=200,
+        preloaded_pending_rewards=200,  # pending rewards for Fatface/FT1 retailers
+        jigsaw_pending_rewards=200,  # pending rewards for ASOS/FT2 retailers
         spare_rewards=50,
         transactions=2000,
         reward_updates=0,
